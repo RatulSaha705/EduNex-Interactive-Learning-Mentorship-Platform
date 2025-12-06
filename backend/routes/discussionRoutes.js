105d2b39110869c9e11e0261261f569ea13f6b14
RatulSaha705 <ratulsaha705@gmail.com>
2025-12-07 03:07:13 +0600

    Updated: Updated discussionRoutes  with reply , upvote,markhelpful functions

--- backend/routes/discussionRoutes.js
+++ backend/routes/discussionRoutes.js
@@ -3,9 +3,14 @@ import { protect, authorizeRoles } from "../middleware/authMiddleware.js";
 import {
   createQuestion,
   getCourseQuestions,
+  createAnswer,
+  getQuestionAnswers,
+  upvoteAnswer,
+  markAnswerHelpful,
 } from "../controllers/discussionController.js";
 
 const router = express.Router();
+// ------------------QUESTIONS-----------------------//
 
 // Create a new question in a specific course (students only)
 router.post(
@@ -17,5 +22,25 @@ router.post(
 
 // Get all questions for a specific course (any logged-in user)
 router.get("/courses/:courseId/questions", protect, getCourseQuestions);
+
+// -------------------- ANSWERS (REPLIES) -------------------- //
+
+router.post(
+  "/questions/:questionId/answers",
+  protect,
+  authorizeRoles("student", "instructor"),
+  createAnswer
+);
+
+// Get all answers for a question (any logged-in user)
+router.get("/questions/:questionId/answers", protect, getQuestionAnswers);
+
+
+// Upvote an answer (any logged-in user)
+router.post("/answers/:answerId/upvote", protect, upvoteAnswer);
+
+// Mark an answer as helpful / best answer
+router.post("/answers/:answerId/mark-helpful", protect, markAnswerHelpful);
+
 
 export default router;
